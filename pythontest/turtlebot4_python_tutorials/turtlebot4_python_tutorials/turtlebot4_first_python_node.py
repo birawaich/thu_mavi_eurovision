8095faf23549d3339333f2c07a55f4d919111e40
birawaich <27787788+birawaich@users.noreply.github.com>
2024-10-23 13:33:34 +0800

    add disco mode i.e. finish tutorial

--- pythontest/turtlebot4_python_tutorials/turtlebot4_python_tutorials/turtlebot4_first_python_node.py
+++ pythontest/turtlebot4_python_tutorials/turtlebot4_python_tutorials/turtlebot4_first_python_node.py
@@ -18,11 +18,77 @@ class TurtleBot4FirstNode(Node):
             qos_profile_sensor_data)
         self.get_logger().info('I was spawned! Good morning!')
 
-    # Interface buttons subscription callback #TODO debug, this is not working :'(
+        # Create a publisher for the /cmd_lightring topic
+        self.lightring_publisher = self.create_publisher(
+            LightringLeds,
+            '/cmd_lightring',
+            qos_profile_sensor_data)
+
+    # Interface buttons subscription callback
+    # check available topics with `ros2 topics list` <-- make sure connected to the right network
+    # buttons are /interface_buttons
+    # note: wait a few seconds until the callback function registers!
     def interface_buttons_callback(self, create3_buttons_msg: InterfaceButtons):
+        self.get_logger().info('\tbutton callback - I am alive!')
         # Button 1 is pressed
         if create3_buttons_msg.button_1.is_pressed:
             self.get_logger().info('Button 1 Pressed!')
+            self.disco()
+
+    # disco function from tutorial
+    def disco(self):
+        # Create a ROS 2 message
+        lightring_msg = LightringLeds()
+        # Stamp the message with the current time
+        lightring_msg.header.stamp = self.get_clock().now().to_msg()
+
+        # Lights are currently off
+        if not self.lights_on_:
+            # Override system lights
+            lightring_msg.override_system = True
+
+            # LED 0
+            lightring_msg.leds[0].red = 255
+            lightring_msg.leds[0].blue = 0
+            lightring_msg.leds[0].green = 0
+
+            # LED 1
+            lightring_msg.leds[1].red = 0
+            lightring_msg.leds[1].blue = 255
+            lightring_msg.leds[1].green = 0
+
+            # LED 2
+            lightring_msg.leds[2].red = 0
+            lightring_msg.leds[2].blue = 0
+            lightring_msg.leds[2].green = 255
+
+            # LED 3
+            lightring_msg.leds[3].red = 255
+            lightring_msg.leds[3].blue = 255
+            lightring_msg.leds[3].green = 0
+
+            # LED 4
+            lightring_msg.leds[4].red = 255
+            lightring_msg.leds[4].blue = 0
+            lightring_msg.leds[4].green = 255
+
+            # LED 5
+            lightring_msg.leds[5].red = 0
+            lightring_msg.leds[5].blue = 255
+            lightring_msg.leds[5].green = 255
+        # Lights are currently on
+        else:
+            # Disable system override. The system will take back control of the lightring.
+            lightring_msg.override_system = False
+
+        # Publish the message
+        self.lightring_publisher.publish(lightring_msg)
+        # Toggle the lights on status
+        self.lights_on_ = not self.lights_on_
+        # Publish the message
+        self.lightring_publisher.publish(lightring_msg)
+
+
 
 
 def main(args=None):
