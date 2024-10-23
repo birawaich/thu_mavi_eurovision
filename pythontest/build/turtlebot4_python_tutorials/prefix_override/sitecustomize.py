6d9c33e2e7ef012593d9599100b041388cd70248
birawaich <27787788+birawaich@users.noreply.github.com>
2024-10-23 13:33:08 +0800

    clean up repo

--- pythontest/build/turtlebot4_python_tutorials/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/bstadler/repositories/thu_mavi_eurovision/pythontest/install/turtlebot4_python_tutorials'
